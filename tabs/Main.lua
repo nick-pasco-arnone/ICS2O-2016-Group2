d5304f287478383d75152a30bb3bbd71e217a471
Jeremy-Dwyer at codea-scm <Jeremy-Dwyer@codea-scm.aws.mapote.com>
2016-11-21 16:22:40 +0000

    added game logo and company logo.

--- tabs/Main.lua
+++ tabs/Main.lua
@@ -28,10 +28,10 @@ function setup()
     pushStyle()    
     
     -- create the scenes
-    Scene("companyScene", CompanyLogoScene)
-    -- delete this line and add in the rest of your scenes
+    Scene("companyLogoScene", CompanyLogoScene)
+    Scene("gameLogoScene", GameLogoScene)
     
-    Scene.Change("companyScene")
+    Scene.Change("companyLogoScene")
 end
 
 -- This function gets called once every frame
